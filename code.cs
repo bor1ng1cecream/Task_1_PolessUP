dae587b170ea6eb95c2e171a058b257ba6aafae9
bor1ng1cecream <127884041+bor1ng1cecream@users.noreply.github.com>
2023-04-09 20:16:53 +0300

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("    +\"\"\"\"\"+");
+        Console.WriteLine("   (| o o |)");
+        Console.WriteLine("    |  ^  |");
+        Console.WriteLine("    | '-' |");
+        Console.WriteLine("    +-----+");
+    }
+}
